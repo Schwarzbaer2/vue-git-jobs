a6c186bbf37a9148a4d5d0a6793eb339d625e900
Andreas Schwarz <andreas.schwarz20@posteo.de>
2021-04-08 10:12:14 +0200

    Adding the header background

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -13,7 +13,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .header {
-  background: $primary-violet;
+  height: 8.5rem;
+  width: 100%;
+  padding: 2rem 1.5rem 0rem 1.5rem;
+  background-image: url("../assets/mobile/bg-pattern-header.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
 
   &__title {
     color: $secondary-white;
