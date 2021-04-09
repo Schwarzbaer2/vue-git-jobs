a3b185a14c21d9b1130b2b1cb9f0dd21bb70a0a6
Andreas Schwarz <andreas.schwarz20@posteo.de>
2021-04-09 13:24:03 +0200

    Adding GitHub Job API to MainCard Component

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,16 +1,17 @@
 <template>
   <article>
     <div class="card">
-      <img class="card__image" src="https://via.placeholder.com/50" alt="" />
-      <p class="card__info">5h ago &bull; Full Time</p>
+      <img class="card__image" :src="job.company_logo" :alt="job.company" />
+      <p class="card__info">5h ago &bull; {{ job.type }}</p>
       <div class="card__title">
-        <h3>Senior Software Engineer</h3>
+        <h3 v-if="job.title.length < 20">{{ job.title }}</h3>
+        <h3 v-else>{{ job.title.substring(0, 23) + ".." }}</h3>
       </div>
       <div class="card__firm">
-        <p>So Digital Inc.</p>
+        <p>{{ job.company }}</p>
       </div>
       <div class="card__location">
-        <h4>Remote, Seoul, Tokyo, Mountain View, San Fransisco</h4>
+        <h4>{{ job.location }}</h4>
       </div>
     </div>
   </article>
@@ -18,7 +19,12 @@
 
 <script>
 export default {
-  name: "MainCard",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
 };
 </script>
 
@@ -28,10 +34,18 @@ export default {
   width: 20rem;
   padding: 0 2rem;
   background: $secondary-white;
+  cursor: pointer;
+
+  text-overflow: ellipsis;
 
   &__image {
     transform: translateY(-50%);
+    z-index: 2;
+    max-height: 50px;
+    max-width: 16rem;
     border-radius: 0.938rem;
+    box-shadow: 0px 5px 13px -7px #000000, 5px 5px 15px 5px rgba(0, 0, 0, 0);
+    object-fit: scale-down;
   }
 
   &__info {
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,26 +1,41 @@
 <template>
   <main class="main">
     <div class="main__grid">
-      <MainCard />
-      <MainCard />
+      <MainCard v-for="job in jobs" :key="job.id" :job="job" />
     </div>
   </main>
 </template>
 
 <script>
 import MainCard from "./MainCard.vue";
+import axios from "axios";
 
 export default {
   name: "TheMain",
   components: {
     MainCard,
   },
+  data() {
+    return {
+      jobs: null,
+    };
+  },
+  created() {
+    axios
+      .get("https://cors.bridged.cc/https://jobs.github.com/positions.json")
+      .then((response) => {
+        this.jobs = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 };
 </script>
 
 <style lang="scss">
 .main {
-  height: 100vh;
+  height: auto;
   padding-top: 4.5rem;
   background: $secondary-lightGrey;
 
